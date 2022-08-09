533ae5b2200dcb9ee03b661a430a70d07048c552
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-08-09 12:48:28 +0500

    author resource

--- Modules/Book/Transformers/AuthorResource.php
+++ Modules/Book/Transformers/AuthorResource.php
@@ -14,6 +14,9 @@ class AuthorResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname
+        ];
     }
 }
