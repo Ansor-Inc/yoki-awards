fc79d697df90aa5e79f69adbbcd3b58323a5f94c
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-08-12 11:03:42 +0500

    fixes

--- Modules/Book/Transformers/GenreResource.php
+++ Modules/Book/Transformers/GenreResource.php
@@ -9,7 +9,7 @@ class GenreResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request
+     * @param \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use App\Traits\HasFilesTrait;
 use Brackets\Media\HasMedia\HasMediaCollectionsTrait;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 
