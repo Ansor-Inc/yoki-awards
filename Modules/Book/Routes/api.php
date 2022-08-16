69928def9d892bdc90e2145f2b9a785c31a88798
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-08-16 09:47:02 +0500

    book comments implemented

--- Modules/Book/Routes/api.php
+++ Modules/Book/Routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Modules\Book\Http\Controllers\BookCommentController;
 use Modules\Book\Http\Controllers\BookController;
 use Modules\Book\Http\Controllers\GenreController;
 use Modules\Book\Http\Controllers\PublisherController;
@@ -21,13 +22,25 @@ Route::get('/publishers', [PublisherController::class, 'index']);
 Route::get('/publishers/{id}', [PublisherController::class, 'show']);
 Route::get('/publishers/{id}/books', [PublisherController::class, 'publisherBooks']);
 
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-
 Route::get('/genres', [GenreController::class, 'index']);
 Route::get('/genres/{genre}/books', [GenreController::class, 'genreBooks']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/books/{book}/bookmark', [BookController::class, 'bookmark']);
-    Route::put('/books/{book}/rate', [BookController::class, 'rate']);
+Route::prefix('books')->group(function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/{book}', [BookController::class, 'show']);
+    Route::get('/{book}/comments', [BookCommentController::class, 'index']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::put('/{book}/bookmark', [BookController::class, 'bookmark']);
+        Route::put('/{book}/rate', [BookController::class, 'rate']);
+        Route::post('/{book}/comments', [BookCommentController::class, 'store']);
+
+    });
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/comments/{comment}', [BookCommentController::class, 'update']);
+    Route::delete('/comments/{comment}', [BookCommentController::class, 'destroy']);
+
+});
+
