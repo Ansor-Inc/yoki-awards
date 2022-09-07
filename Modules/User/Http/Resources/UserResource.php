b08f0dae355984b7f6694ad236f251c15067662c
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-09-07 16:46:12 +0500

    filesystem changed

--- Modules/User/Http/Resources/UserResource.php
+++ Modules/User/Http/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'fullname' => $this->fullname,
-            'avatar' => $this->avatar,
+            'avatar' => url($this->avatar),
             'phone' => $this->phone,
             'gender' => $this->gender,
             'birthdate' => $this->birthdate,
