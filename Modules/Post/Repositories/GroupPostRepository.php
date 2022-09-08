6cf6c8b80b1cf85de85c7972b013699b8c52912b
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-09-08 12:04:42 +0500

    post fixes

---
+++ Modules/Post/Repositories/GroupPostRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Post\Repositories;
+
+use Modules\Post\Repositories\Interfaces\GroupPostRepositoryInterface;
+
+class GroupPostRepository implements GroupPostRepositoryInterface
+{
+
+}
