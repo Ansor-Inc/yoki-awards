973dfdc24c58b8c0402fe7aad0045ceee4b997c5
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-09-17 11:16:29 +0500

    some fixes

--- Modules/Book/Repositories/GenreRepository.php
+++ Modules/Book/Repositories/GenreRepository.php
@@ -14,7 +14,10 @@ class GenreRepository implements GenreRepositoryInterface
 
     public function getGenreBooks(Genre $genre, $perPage = 0)
     {
-        $query = $genre->books()->onlyListingFields()->with('author:id,firstname,lastname');
+        $query = $genre->books()
+            ->onlyListingFields()
+            ->with('author:id,firstname,lastname')
+            ->withAvg('bookUserStatuses as rating', 'rating');
 
         return $perPage == 0 ? $query->limit(100)->get() : $query->paginate($perPage);
     }
