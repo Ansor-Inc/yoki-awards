8365bf97d255c941a193258a19d1668485d2d601
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-10-20 10:35:14 +0500

    fixed path generator

---
+++ config/App/Components/Spatie/CustomPathGenerator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Components\Spatie;
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+
+class CustomPathGenerator implements PathGenerator
+{
+    /*
+       * Get the path for the given media, relative to the root storage path.
+       */
+    public function getPath(Media $media): string
+    {
+        return $this->getBasePath($media) . '/';
+    }
+
+    /*
+     * Get the path for conversions of the given media, relative to the root storage path.
+     */
+    public function getPathForConversions(Media $media): string
+    {
+        return $this->getBasePath($media) . '/conversions/';
+    }
+
+    /*
+     * Get the path for responsive images of the given media, relative to the root storage path.
+     */
+    public function getPathForResponsiveImages(Media $media): string
+    {
+        return $this->getBasePath($media) . '/responsive-images/';
+    }
+
+    /*
+     * Get a unique base path for the given media.
+     */
+    protected function getBasePath(Media $media): string
+    {
+        $prefix = config('media-library.prefix', '');
+        $path = Str::plural($media->model_type) . '/' . $media->getKey();
+
+        if ($prefix !== '') {
+            return $prefix . '/' . $path;
+        }
+
+        return $path;
+    }
+}
