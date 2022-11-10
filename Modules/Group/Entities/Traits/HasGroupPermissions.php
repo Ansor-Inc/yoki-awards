46502c80c6287f2157d24519d4fbd5a2be966fb2
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-11-10 17:09:13 +0500

    some fixes

--- Modules/Group/Entities/Traits/HasGroupPermissions.php
+++ Modules/Group/Entities/Traits/HasGroupPermissions.php
@@ -15,12 +15,12 @@ trait HasGroupPermissions
             ->leftJoin('group_admins', 'memberships.id', '=', 'group_admins.membership_id')
             ->leftJoin('black_list', 'memberships.id', '=', 'black_list.membership_id');
     }
-    
+
     public function getCurrentUserPermissionsAttribute()
     {
         $permissions = [
             'can_update_group' => false,
-            'can_create_post' => false,
+            'can_create_post' => true,
             'can_add_to_blacklist' => false,
             'can_comment' => true,
             'can_see_post' => true,
