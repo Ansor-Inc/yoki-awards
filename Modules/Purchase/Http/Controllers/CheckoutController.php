36f15bba399a93980dbf3d58d61ce80757146bef
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2022-12-22 16:51:55 +0500

    fixes

--- Modules/Purchase/Http/Controllers/CheckoutController.php
+++ Modules/Purchase/Http/Controllers/CheckoutController.php
@@ -13,7 +13,7 @@ class CheckoutController extends Controller
         if ($purchase->state === PurchaseStatus::PENDING_PAYMENT->value)
             return response(["checkout_link" => $this->generateCheckoutLinkForPayme($purchase)]);
 
-        return response(['message' => 'Invalid purchase! Completed or canceled purchcase!']);
+        return response(['message' => 'Invalid purchase! Completed or canceled purchcase!'],500);
     }
 
     public function generateCheckoutLinkForPayme($purchase)
