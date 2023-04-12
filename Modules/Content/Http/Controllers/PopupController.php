21bfeb0465ad8cb97a84e6685e85cf117a26cbf2
Bakhtiyor <alijanovbakhtiyor@gmail.com>
2023-04-12 10:29:43 +0500

    some fixes

--- Modules/Content/Http/Controllers/PopupController.php
+++ Modules/Content/Http/Controllers/PopupController.php
@@ -3,13 +3,12 @@
 namespace Modules\Content\Http\Controllers;
 
 use Illuminate\Routing\Controller;
-use Modules\Content\Transformers\PopupResource;
 
 class PopupController extends Controller
 {
     public function index()
     {
-        return PopupResource::make([
+        return collect([
             'title' => setting('popup_title'),
             'body' => setting('popup_body'),
             'code' => setting('popup_code')
